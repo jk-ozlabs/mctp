28e7fcf0baccc9b59b148f0df7214c57b3d0d03e
Jeremy Kerr <jk@codeconstruct.com.au>
2025-08-25 16:22:11 +0800

    mctp-bench: (actually) address unused getrandom() result
    Turns out warn_unused_result wins over a (void) cast. Begrudgingly add
    an error check.
    
    Signed-off-by: Jeremy Kerr <jk@codeconstruct.com.au>

--- src/mctp-bench.c
+++ src/mctp-bench.c
@@ -686,7 +686,9 @@ static int command(mctp_eid_t eid, int net, enum command command,
 	/* on a recent kernel (ie., any with MCTP support), getrandom is
 	 * infallible with <=256-byte lengths
 	 */
-	(void)getrandom(&req->iid, sizeof(req->iid), 0);
+	ssize_t len = getrandom(&req->iid, sizeof(req->iid), 0);
+	if (len != sizeof(req->iid))
+		err(EXIT_FAILURE, "getrandom");
 	memcpy(req->body, body, body_len);
 
 	rc = sendto(sd, req, req_len, 0, (struct sockaddr *)&addr,
